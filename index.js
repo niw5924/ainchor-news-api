182473f64b7f3a8a958403560e3dcb871466f320
niw0906 <niw0906@naver.com>
2025-11-03 22:37:11 +0900

    feat: /api/brief-tts/convert 엔드포인트 추가 및 라우터 연결

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const briefTTSRoutes = require('./routes/brief_tts');
 
 const app = express();
+
+app.use(express.json());
+app.use('/api/brief-tts', briefTTSRoutes);
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('ainchor_news_api on http://0.0.0.0:3000');
